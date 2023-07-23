19887dc455491e3fa44f9eaf82dff15ec1b8b867
luoyesiqiu <yanwendream@163.com>
2023-07-23 23:45:00 +0800

    Support multi-thread calls

--- safecall/src/main/c/include/safe_call.h
+++ safecall/src/main/c/include/safe_call.h
@@ -5,6 +5,8 @@
 #ifndef SAFECALLER_SAFE_CALL_H
 #define SAFECALLER_SAFE_CALL_H
 
+#include <pthread.h>
+
 #ifdef __cplusplus
 extern "C"  {
 #endif
@@ -12,24 +14,30 @@ extern "C"  {
 JNIEXPORT extern int before_call(const void *func_addr);
 JNIEXPORT extern void after_call(const void *func_addr);
 
+pthread_mutex_t safe_call_mutex = PTHREAD_MUTEX_INITIALIZER;
+
 #define SAFE_CALL(func_addr,ret,...) do { \
-int call = before_call(func_addr); \
+    pthread_mutex_lock(&safe_call_mutex);             \
+    int call = before_call(func_addr); \
     if(call) {                           \
         ret = func_addr(__VA_ARGS__);                 \
     }                                         \
     if(call) { \
         after_call(func_addr);                    \
-    }                                          \
+    }                                     \
+	pthread_mutex_unlock(&safe_call_mutex); \
 } while(0)
 
 #define SAFE_CALL_VOID(func_addr,...) do { \
+    pthread_mutex_lock(&safe_call_mutex);             \
     int call = before_call(func_addr); \
     if(call) {                           \
         func_addr(__VA_ARGS__);                 \
     }                                         \
     if(call) { \
         after_call(func_addr);                    \
-    }                                          \
+    }                                      \
+	pthread_mutex_unlock(&safe_call_mutex); \
 } while(0)
 
 #ifdef __cplusplus
